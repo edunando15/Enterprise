47859e5e450b6b588c6f718a8285bb8f23c86880
bia104 <biancamaria.cerino@studenti.unicam.it>
2024-03-13 19:37:33 +0100

    [add] Aggiunta configurazione di BookCategory. Aggiunta configurazione di User.

---
+++ Enterprise/Configurations/BookCategoryConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Model.Entities;
+
+namespace Model.Configurations
+{
+    internal class BookCategoryConfiguration : IEntityTypeConfiguration<BookCategory>
+    {
+
+        public void Configure(EntityTypeBuilder<BookCategory> builder)
+        {
+            builder.ToTable("BookCategory");
+            builder.HasKey(b => new { b.BookId, b.CategoryId });
+        }
+    }
+}
---
+++ Enterprise/Configurations/UserConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Model.Entities;
+
+namespace Model.Configurations
+{
+    internal class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("User");
+            builder.HasKey(u => u.Id);
+            builder.Property(u => u.Name).HasMaxLength(50);
+            builder.Property(u => u.Email).HasMaxLength(50);
+            builder.Property(u => u.Password).HasMaxLength(50);
+        }
+    }
+}
